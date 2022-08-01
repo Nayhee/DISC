74509b70c7d4fee299e2190c428d9a56808d698e
Nayhee <nathantraczewski@gmail.com>
2022-08-01 14:47:05 -0500

    updated login and register css

--- DISC/client/src/components/auth/Register.js
+++ DISC/client/src/components/auth/Register.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { useNavigate } from "react-router-dom";
 import { register } from "../modules/authManager";
+import "./Register.css";
+
 
 export default function Register() {
   const navigate = useNavigate();
@@ -25,44 +27,48 @@ export default function Register() {
   };
 
   return (
-    <Form onSubmit={registerClick}>
-      <fieldset>
-        <FormGroup>
-          <Label htmlFor="Name">Name</Label>
-          <Input
-            id="Name"
-            type="text"
-            onChange={(e) => setName(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="email">Email</Label>
-          <Input
-            id="email"
-            type="text"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input
-            id="password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="confirmPassword">Confirm Password</Label>
-          <Input
-            id="confirmPassword"
-            type="password"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Button>Register</Button>
-        </FormGroup>
-      </fieldset>
-    </Form>
+    <div className="registerWrapper">
+        <Form className="registerForm" onSubmit={registerClick}>
+          <h2 className="registerHeader">Register</h2>
+          
+          <fieldset>
+            <FormGroup>
+              <Label htmlFor="Name">Name</Label>
+              <Input
+                id="Name"
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                id="email"
+                type="text"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="confirmPassword">Confirm Password</Label>
+              <Input
+                id="confirmPassword"
+                type="password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Button color="primary">Register</Button>
+            </FormGroup>
+          </fieldset>
+        </Form>
+    </div>
   );
 }
